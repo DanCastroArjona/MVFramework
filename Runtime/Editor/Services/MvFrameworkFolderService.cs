54fe1d7df0fc7f879e27cab0b69bcfa9d6781cfa
Daniel Castro <danicastroa@gmail.com>
2024-09-08 14:09:55 +0200

    MVC and MVP pattern for Unity

---
+++ Runtime/Editor/Services/MvFrameworkFolderService.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+using MVFramework.Editor.FolderRepositories;
+using UnityEditor;
+
+namespace MVFramework.Editor.Services
+{
+    public class MvcFolderService : FolderGeneratorService<MvcFolders>
+    {
+        [MenuItem("Assets/Create/Mvc/Generate MVC Folders")]
+        private static void GenerateMVCFoldersInPath()
+        {
+            GenerateFoldersInSelectedGuidPath();
+        }
+    }
+    
+    public class MvpFolderService : FolderGeneratorService<MvpFolders>
+    {
+        [MenuItem("Assets/Create/Mvc/Generate MVP Folders")]
+        private static void GenerateMVPFoldersInPath()
+        {
+            GenerateFoldersInSelectedGuidPath();
+        }
+    }
+}
+#endif
